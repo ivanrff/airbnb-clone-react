ced4f83852620435f481da4d6edb699406dd8263
Ivan Ramos <eng.ivanrff@gmail.com>
2022-11-10 09:53:00 -0300

    navbar-shadows

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img className='navbar--logo' src="../public/airbnb-logo.png" alt=""/>
+        </nav>
+    )
+    
+}
+
+export default Navbar;
